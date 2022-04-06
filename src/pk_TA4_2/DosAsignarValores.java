bd79151bc431bba0a3ea1c69bd1dd2d7c44220f9
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-06 13:02:03 +0200

    Update putno dos

--- src/pk_TA4_2/DosAsignarValores.java
+++ src/pk_TA4_2/DosAsignarValores.java
@@ -3,19 +3,19 @@ package pk_TA4_2;
 public class DosAsignarValores {
 
 	public static void main(String[] args) {
-		// Declarar diferentes variables
+		
+		// Declarar y mostrar por pantalla las diferentes variables
+		
 		int N = 1;
 		double A = 2.2;
-		char C = 'A';
-		
-		// Mostrar por pantalla
-		
-		System.out.println(N);
-		System.out.println(A);
-		System.out.println(C);
-		System.out.println(N + A);
-		System.out.println(A - N);
-		System.out.println((int)C);
+		char C = 'C';
+				
+		System.out.println("Variable N = " + N);
+		System.out.println("Variable A = " + A);
+		System.out.println("Variable C = " + C);
+		System.out.println(N + "+" + A + "=" + (N + A));
+		System.out.println(A + "-" + N + "=" + (A - N));
+		System.out.println("Valor numérico del caracter 'C' = " + (int)C);
 		
 
 	}
