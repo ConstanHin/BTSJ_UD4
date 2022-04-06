c6d6bc52bf312d52af996f6d15675a92d2093c2d
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-06 11:38:17 +0200

    Primer y segundo ejercicio

---
+++ src/pk_TA4_1/Uno.java
@@ -0,0 +1,21 @@
+package pk_TA4_1;
+
+public class Uno {
+
+	public static void main(String[] args) {
+		
+		
+		int number1 = 1;
+		int number2 = 2;
+		
+		// Muestra por pantalla suma, resta, multiplicacióm, división y módulo
+		
+		System.out.println(number1 + number2);
+		System.out.println(number1 - number2);
+		System.out.println(number1 * number2);
+		System.out.println(number1 / number2);
+		System.out.println(number1 % number2);
+
+	}
+
+}
---
+++ src/pk_TA4_2/DosAsignarValores.java
@@ -0,0 +1,23 @@
+package pk_TA4_2;
+
+public class DosAsignarValores {
+
+	public static void main(String[] args) {
+		// Declarar diferentes variables
+		int N = 1;
+		double A = 2.2;
+		char C = 'A';
+		
+		// Mostrar por pantalla
+		
+		System.out.println(N);
+		System.out.println(A);
+		System.out.println(C);
+		System.out.println(N + A);
+		System.out.println(A - N);
+		System.out.println((int)C);
+		
+
+	}
+
+}
